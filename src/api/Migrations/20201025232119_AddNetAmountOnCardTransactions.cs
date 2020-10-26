27d6fe2b5972d3c0bab09658dceb74208d53b1e6
Andre Lucas <andre.job95@gmail.com>
2020-10-25 21:46:03 -0300

    Add net amount column pending migration on card transaction

---
+++ src/api/Migrations/20201025232119_AddNetAmountOnCardTransactions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class AddNetAmountOnCardTransactions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "NetAmount",
+                table: "CardTransactions",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NetAmount",
+                table: "CardTransactions");
+        }
+    }
+}
